a51c223176c8260cdeb47ce217e06687d5d5ea46
Tim Bishop <tim.bishop@students.mq.edu.au>
2017-02-04 13:44:36 +1100

    changes

--- src/main/java/com/nms/app/MenuItems.java
+++ src/main/java/com/nms/app/MenuItems.java
@@ -1,13 +1,15 @@
 package com.nms.app;
 
 /**
+ * Commenting provided for the benefit of Andrew Whiskin
  * Class to contain the various menu items and their ingredients present on the menu.
  * Once all orders have been processed, print out all of the data contained in here to a PDF file.
+ * 
  * CRITICAL - The data must be summarized via three functions, to enable easy order fulfillment:
  * 1. All Orders - Print out all of the menu item quantities - needed for purchasing
  * 2. All Pizzas - Print out of all pizzas ordered - purchasing
  * 3. All Sandwhiches -More complex. Will need to use the data contained in the Orders class. 
- * 						As the orders are being read, each time a Sandwhich is listed, create a ne
+ * 						As the orders are being read, each time a Sandwhich is listed, create a new Sandwhich instance
  * @author tim.bishop
  *
  */
